ec60b548ed11ebfb6ffaf67463074fe6964443ce
deluscc4 <delusccafs@icloud.com>
2023-06-10 22:38:40 -0300

    header

---
+++ main.lua
@@ -0,0 +1,34 @@
+-- Habilitar o UTF-8 no terminal
+os.execute("chcp 65001")
+os.execute("cls")
+
+-- Header
+print([[
+=======================================================================
+
+                      /()
+                     / /
+                    / /
+      /============| |------------------------------------------,
+    {=| / / / / / /|()}     }     }     }                        >
+      \============| |------------------------------------------'
+                    \ \
+                     \ \
+                      \()
+
+                        ---------------------------
+
+                        ⚔️  SIMULADOR DE BATALHA ⚔️            
+
+=======================================================================
+            Você empunha seu artefato e se prepara para lutar.
+                         É hora da batalha!
+]])
+
+-- Obter definição do jogador
+
+-- Obter definição do monstro
+
+-- Apresentar o monstro
+
+-- Começar o loop de batalha
